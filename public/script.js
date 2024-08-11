6bbf62a8621fa654a0429776edb7b564d0f76a66
Alp <gymifyturkey@gmail.com>
2024-08-11 15:48:41 +0300

    NFT Contrat and basic UI

---
+++ public/script.js
@@ -0,0 +1,9 @@
+document.getElementById('worldcoin-auth-btn').addEventListener('click', function() {
+    alert('Worldcoin authentication initiated!');
+    // Worldcoin API integration code will go here
+});
+
+document.getElementById('checker-nft-btn').addEventListener('click', function() {
+    alert('Checker NFT initiated!');
+    // NFT checker functionality code will go here
+});
