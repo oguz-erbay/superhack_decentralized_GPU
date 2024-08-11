436cc01a4f02ce65f9ff4952dd874f24974aa76c
gymifyturkey-admin <gymifyturkey@gmail.com>
2024-08-11 18:22:33 +0300

    GPU added

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WorldIDComponent from "./components/WorldIDComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <WorldIDComponent />
+    </div>
+  );
+}
+
+export default App;
